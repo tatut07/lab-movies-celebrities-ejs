4b63dd7b5f6acbef465b710edf40b9b95278dc21
Tatiana Toledo <tatiana@tatianas-mbp.speedport.ip>
2022-10-31 16:11:24 +0100

    iteration2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,22 @@
 //  Add your code here
+
+const { Schema, model } = require("mongoose");
+
+const CelebritySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  catchPhrase: {
+    type: String,
+    required: true,
+  },
+});
+
+const Celebrity = model("celebrity", CelebritySchema);
+
+module.exports = Celebrity;
